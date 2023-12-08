6d71c9b6a6812609b24507f0ff66c92ee3e4da0c
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-12-08 17:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const form=document.getElementById("form");
 let exicute=(event)=>
 	{
 		event.preventDefault();
-		alert(event.target.LastName.value,event.target.FirstName.value);
+		alert(event.target.FirstName.value);
+		alert(event.target.LastName.value);
 		alert(event.target.LastName.value);
 		alert(event.target.PhoneNumber.value);
 		alert(event.target.EmailID.value);
